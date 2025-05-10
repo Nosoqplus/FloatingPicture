85b9d03b5914289fe3e74f2c002ac00cf9f8c3fa
Vadimii <vadimsemenovvs5@gmail.com>
2025-05-10 17:50:36 +0600

    bug fixed

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const buttonRight = document.querySelector('.turn-right');
     const buttonLeft = document.querySelector('.turn-left');
     
-    const BUTTON_SWAP_COOLDOWN = 500;
+    const BUTTON_SWAP_COOLDOWN = 100;
     
     let currPage;
     let nextPage;
@@ -23,7 +23,25 @@ document.addEventListener('DOMContentLoaded', function(){
     let currPageIndex = 0;
     console.log(pages, pagesCircles, buttonLeft, buttonRight, contentDiv);
     
-    // Starts Picture in Picture
+    // Вспомогательные функции
+    function appendMultipleChildren(parent, children = []){
+        children.forEach((elem) => {
+            parent.appendChild(elem);
+        });
+    }
+
+    // Включение/выключение кнопок
+    function toggleButtons(buttons = []){
+        buttons.forEach((button) =>{
+            button.disabled = !button.disabled;
+        });
+    }
+
+    
+    
+    
+    // Функционал плавающего окна
+    
     async function startPicInPic(){
         try {
             let mediaStream = await navigator.mediaDevices.getDisplayMedia();
@@ -37,103 +55,106 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
     
-    // Page Swap
-    function spawnPageCircles(){
-        const pageCircleContainer = document.querySelector('.page-circle-container');
-        pages.forEach(() =>{
-            circle = document.createElement('div');
-            circle.classList.add('page-circle');
+    // Функционал инструкции
 
-            pageCircleContainer.appendChild(circle);
-            pagesCircles.push(circle);
+    // Создает пипсы для обозначения настоящей страницы
+    // Выполняется один раз при загрузке страницы
+    function spawnPipsCircles(){
+        // Получение в переменную контейнера, куда они будут создаваться
+        const pipsContainer = document.querySelector('.pips-container');
+        
+        // Для каждой страницы создается новая пипса и добавляется в контейнер в DOM, а так же в массив со всеми пипсами
+        // для дальнейших операций
+        pages.forEach(() =>{
+            pip = document.createElement('div');
+            pip.classList.add('pip');
+            
+            pipsContainer.appendChild(pip);
+            pagesCircles.push(pip);
         });
     }
 
-    function getPagesReadyToSwap(){
-        if(!currPage){
-            currPage = pages[currPageIndex].cloneNode(true);
-        }
-        nextPage = pages[currPageIndex >= pages.length - 1 ? 0 : currPageIndex + 1 ].cloneNode(true);
-        prevPage = pages[currPageIndex <= 0 ? pages.length - 1 : currPageIndex - 1 ].cloneNode(true);
-        
-        contentDiv.appendChild(currPage);
-        contentDiv.appendChild(nextPage);
-        contentDiv.appendChild(prevPage);
-        
-        currPage.style.left = 0;
-        nextPage.style.left = '100%';
-        prevPage.style.left = '-100%';
-        
-        console.log(prevPage, currPage, nextPage, currPageIndex);
-        PageCirclesUpdate();
-    }
-    
-    function PageCirclesUpdate(){
+    // Выключает все пипсы и включает ту, индекс в массиве которой соответствует индексу в массиве настоящей страницы
+    function pipsUpdate(){
         pagesCircles.forEach((circle) => {
             circle.classList.remove('active');
         })
 
         pagesCircles[currPageIndex].classList.add('active');
     };
-
-    function toggleButtons(){
-        buttonLeft.disabled = !buttonLeft.disabled;
-        buttonRight.disabled = !buttonRight.disabled;
-    }
-
-    buttonRight.addEventListener('click', () => {
-
-        currPage.style.left = '-100%';
-        nextPage.style.left = 0;
-
+    
+    // Подготавливаем страницы к взаимодействию с пользователем
+    // Выполняется при загрузке страницы и каждый раз после нажатия кнопки
+    function getPagesReadyToSwap(){
+        // Создает страницу, отображаемую пользователю, если таковая отсутствует
+        if(!currPage){
+            currPage = pages[currPageIndex].cloneNode(true);
+        }
+        // Заранее создается следующая и предыдущая страницы, скрытые от пользователя
+        nextPage = pages[currPageIndex >= pages.length - 1 ? 0 : currPageIndex + 1 ].cloneNode(true);
+        prevPage = pages[currPageIndex <= 0 ? pages.length - 1 : currPageIndex - 1 ].cloneNode(true);
         
-        currPageIndex++;
+        // Расстановка созданных страниц
+        currPage.style.left = 0;
+        nextPage.style.left = '100%';
+        prevPage.style.left = '-100%';
+        
+        // Добавляем страницы в свой контейнер
+        appendMultipleChildren(contentDiv, [currPage, nextPage, prevPage]);
+        
+    }
+    
+    // Смена страниц
+    function swapPages(newCurrPage, oldPrevPage, toLeft){
+        
+        // Изменение положения новой и старой текущей страницы
+        newCurrPage.style.left = 0;
+        // currPage.style.left = toLeft ? '-100%' : '100%' ;
+        
+        // Изменение индекся текущей страницы
+        toLeft ? currPageIndex++ : currPageIndex--;
         if(currPageIndex >= pages.length){
             currPageIndex = 0;
         }
-        toggleButtons()
-        
-        setTimeout(
-            () => {
-                currPage.remove();
-                prevPage.remove();
-                getPagesReadyToSwap();
-                toggleButtons();
-            },
-            BUTTON_SWAP_COOLDOWN
-        );
-        currPage = nextPage;
-        
-    });
-    buttonLeft.addEventListener('click', () => {
-        
-        currPage.style.left = '+100%';
-        prevPage.style.left = 0;
-        
-        
-        if(currPageIndex === 0){
+        if(currPageIndex < 0){
             currPageIndex = pages.length - 1;
-        } else {
-            currPageIndex--;
         }
-        toggleButtons();
+
+        // Обновление пипс. Включение той, что соответствует номеру страницы
+        pipsUpdate();
         
-        setTimeout(
-            () => {
-                currPage.remove();
-                nextPage.remove();
-                getPagesReadyToSwap();
-                toggleButtons();
-            },
-            BUTTON_SWAP_COOLDOWN
-        );
-        currPage = prevPage;
+        // Выключение кнопок до момента завершения анимации
+        toggleButtons([buttonLeft, buttonRight]);
         
+        currPage.remove();
+        oldPrevPage.remove();
+        
+        setTimeout(() => {
+            getPagesReadyToSwap();
+            toggleButtons([buttonLeft, buttonRight]);
+        }, BUTTON_SWAP_COOLDOWN);
+
+        console.log(currPageIndex);
+        currPage = newCurrPage;
+        console.log('currpage:', currPage, 'nextpage:', nextPage, 'nextCurrpage:', newCurrPage, 'oldPrevPage', oldPrevPage);
+    }
+    
+    
+    
+    // Прослушиватели событий для кнопок
+    buttonRight.addEventListener('click', () => {
+        swapPages(nextPage,prevPage, true);
     });
 
-
-    // onLoad
+    buttonLeft.addEventListener('click', () => {
+        swapPages(prevPage, nextPage, false);
+    });
+    
     mediaStreamButton.addEventListener('click', startPicInPic);
-    spawnPageCircles();
+    
+    // onLoad
+    spawnPipsCircles();
+    pipsUpdate();
+
     getPagesReadyToSwap();
 });
